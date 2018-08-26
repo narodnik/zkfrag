adab8a50d48bc2c5572fef32d286cc9526065eb1
Amir Taaki <amir@dyne.org>
2018-08-26 13:43:04 +0200

    add some extra variables

--- main.cpp
+++ main.cpp
@@ -69,11 +69,20 @@ int main(int argc, char** argv)
 
     libdark::variables_map variables;
     variables["G"] = bc::ec_point::G;
+    variables["H"] = bc::ec_point(bc::base16_literal(
+        "02a5d7927f021e8561a33c28fb4cc537dfab678ef1709f04002338a5ab6d02316c"));
     variables["d"] = bc::ec_scalar(bc::base16_literal(
         "e3e796dcbd77e3565618d1feeb6ddc921f6531e417ed32182253ed564b764b3b"));
+    variables["z"] = bc::ec_scalar(bc::base16_literal(
+        "afc743a0af634724f4c2e9d3b78a4e2bc5602eb9e038ced88cbd5220d65f07e9"));
+    variables["x"] = bc::ec_scalar(bc::base16_literal(
+        "319e421f7040de652ebbc02782a4a07b0e8323ed95ef1a92c6704e0a6d6612ed"));
     const auto& G = *variables["G"].point();
+    const auto& H = *variables["H"].point();
     const auto& d = *variables["d"].scalar();
+    const auto& x = *variables["x"].scalar();
     variables["Q"] = d * G;
+    variables["Y"] = x * H;
 
     DARK_ASSERT(variables["G"].point());
     DARK_ASSERT(variables["d"].scalar());
