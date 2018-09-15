b2a6aceaf03626ffdde8c4e6f8f9972cdd4b561c
Amir Taaki <amir@dyne.org>
2018-09-15 08:51:08 +0200

    move fragments

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ctags --langmap=C++:+.ipp,Flex:+.l -R .
-flex --outfile=src/sigma/parser/generated/scanner.cpp --header-file=src/sigma/parser/generated/scanner.hpp src/sigma/parser/scanner.l
-bison --defines=src/sigma/parser/generated/parser.hpp --output=src/sigma/parser/generated/parser.cpp src/sigma/parser/parser.y
+#flex --outfile=src/sigma/parser/generated/scanner.cpp --header-file=src/sigma/parser/generated/scanner.hpp src/sigma/parser/scanner.l
+#bison --defines=src/sigma/parser/generated/parser.hpp --output=src/sigma/parser/generated/parser.cpp src/sigma/parser/parser.y
 make install
 ./tools/zkfrag/zkfrag
 notify-send 'Compile Finished' 'Check your Terminal' --icon=dialog-information
