e0ae45ebb9dc4516288a0610522c49cf62bfcbd7
Amir Taaki <amir@dyne.org>
2018-09-21 07:37:04 +0200

    unit test framework.

---
+++ test/main.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE libdark_test
+#include <boost/test/unit_test.hpp>
+
---
+++ test/math/sha256_combine.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+#include <libdark.hpp>
+
+BOOST_AUTO_TEST_SUITE(sha256_combine_tests)
+
+// constructors
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(sha256_combine__default_test)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
