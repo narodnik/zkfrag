218adb5f57f5ce3f1d8d84daf3b6f5b81f517b3c
Amir Taaki <amir@dyne.org>
2018-09-21 10:13:41 +0200

    add headers to makefile

--- include/libdark.hpp
+++ include/libdark.hpp
@@ -1,6 +1,7 @@
 #ifndef LIBDARK_HPP
 #define LIBDARK_HPP
 
+#include <libdark/math/sha256_combine.hpp>
 #include <libdark/sigma/sigma_ast_parser.hpp>
 #include <libdark/sigma/sigma_ast_node.hpp>
 #include <libdark/sigma/variables_map.hpp>
